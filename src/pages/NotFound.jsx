bbc792d3439d76eff55ab8edec8a4b179f4f1819
coolorbi <csaraswat87@gmail.com>
2022-02-13 15:19:05 +0530

    404 image made responsive!!

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,11 @@ const NotFound = () => {
   return (
     <section>
       <div className="container text-center p-5">
-        <img src="/images/404-error.png" alt="Not Found" className="fluid" />
+        <img
+          src="/images/404-error.png"
+          alt="Not Found"
+          className="img-fluid"
+        />
         <h1 className="text-center dispaly-6">Oops! Page not found!!</h1>
       </div>
     </section>
