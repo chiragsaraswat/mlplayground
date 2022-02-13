d4e6a2d3280b3023ac83f267f7ac8095821d7a09
coolorbi <csaraswat87@gmail.com>
2022-02-13 15:25:21 +0530

    repo section tweaked

--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import RepoItem from './RepoItem';
 
 const RepoList = ({ repos }) => {
-  return (
-    <div className="row">
-      {repos.map((repo) => (
-        <RepoItem key={repo.id} repo={repo} />
-      ))}
-    </div>
-  );
+  if (repos.length === 0) {
+    return (
+      <div className="row">
+        <p className="lead text-info">User has no Public Repositories Yet!!</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="row">
+        {repos.map((repo) => (
+          <RepoItem key={repo.id} repo={repo} />
+        ))}
+      </div>
+    );
+  }
 };
 
 export default RepoList;
