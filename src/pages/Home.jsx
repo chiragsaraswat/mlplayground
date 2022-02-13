747662ed1289612f05c136791d85d3777a5cf197
coolorbi <csaraswat87@gmail.com>
2022-02-13 15:47:07 +0530

    form added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,8 @@ import Spinner from '../components/Spinner';
 const Home = () => {
   const [username, setUsername] = useState('');
   const { loading, dispatch, users } = useContext(GithubContext);
-  const onClickHandler = async () => {
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
     dispatch({ type: 'SET_LOADING' });
     const users = await searchUsers(username);
     dispatch({ type: 'SEARCH_USERS', payload: users });
@@ -15,29 +16,31 @@ const Home = () => {
     <>
       <section style={{ minHeight: '70vh' }}>
         <div className="container">
-          <div className="row mt-5">
-            <div className="input-group mb-3 input-group-lg">
-              <input
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                type="text"
-                className="form-control"
-                placeholder="Enter Github Username"
-                aria-label="Recipient's username"
-                aria-describedby="button-addon2"
-              />
-              <div className="input-group-append">
-                <button
-                  className="btn btn-primary"
-                  onClick={onClickHandler}
-                  type="button"
-                  id="button-addon2"
-                >
-                  Search
-                </button>
+          <form onSubmit={onSubmitHandler}>
+            <div className="row mt-5">
+              <div className="input-group mb-3 input-group-lg w-100">
+                <input
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter Github Username"
+                  aria-label="Recipient's username"
+                  aria-describedby="button-addon2"
+                />
+                <div className="input-group-append">
+                  <button
+                    className="btn btn-primary"
+                    type="submit"
+                    id="button-addon2"
+                  >
+                    Search
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
+          </form>
+
           {loading ? (
             <Spinner />
           ) : (
